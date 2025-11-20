204083afb467aa0e848e2390c7e3dae532475da5
Alok Ranjan <iitpatnaalokranjan@gmail.com>
2025-11-20 20:48:46 +0530

    Fix import path for authDataContext

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -3,7 +3,7 @@ import Titles from "../components/Titles";
 import { useState } from "react";
 import { useContext } from "react";
 import { shopDataContext } from "../context/ShopContext";
-import { authDataContext } from "../context/authContext";
+import { authDataContext } from "../context/AuthContext";
 import axios from "axios";
 import { useEffect } from "react";
 
