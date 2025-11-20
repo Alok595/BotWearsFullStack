67243c5da54a9ec81174d5a452cdcd74c999285d
Alok Ranjan <iitpatnaalokranjan@gmail.com>
2025-11-20 20:45:25 +0530

    Fix import case for AuthContext in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AuthContext from "./context/authContext.jsx";
+import AuthContext from "./context/AuthContext.jsx";
 import UserContext from "./context/UserContext.jsx";
 import ShopContext from "./context/ShopContext.jsx";
 
