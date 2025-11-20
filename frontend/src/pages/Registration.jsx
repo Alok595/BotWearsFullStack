0a2300afb5b08ffb365ea2b188020f221aaa8f5e
Alok Ranjan <iitpatnaalokranjan@gmail.com>
2025-11-20 20:49:25 +0530

    Fix import path for authDataContext

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { authDataContext } from "../context/authContext";
+import { authDataContext } from "../context/AuthContext";
 import axios from "axios";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../utils/Firebase";
